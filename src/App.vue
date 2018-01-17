d1b8c075acfbe72f5fc44c0a41d040d0295237d4
Zach Edwards <tolklorist@gmail.com>
2018-01-17 14:00:28 -0600

    dashboard prototype

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<template lang="html">
+    <div id="app">
+      <div class="columns is-mobile">
+  <div class="column">
+    <div class="box">
+  <article class="media">
+    <div class="media-left">
+        <i class="bb-sea bb-5x"></i>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>Seattle Mariners</strong> <br><small>AL West</small><small>  (52-74)</small>
+          <br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+        </p>
+      </div>
+    </div>
+  </article>
+</div>
+  </div>
+  <div class="column">
+    <div class="box">
+  <article class="media">
+    <div class="media-left">
+        <i class="bb-atl bb-5x"></i>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>Atlanta Braves</strong> <br><small>AL West</small><small>  (52-74)</small>
+          <br>
+          Lorem ipsum dolor sit amet, consectetur. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+        </p>
+      </div>
+    </div>
+  </article>
+</div>
+  </div>
+  <div class="column">
+    <div class="box">
+  <article class="media">
+    <div class="media-left">
+        <i class="bb-lad bb-5x"></i>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>Los Angeles Dodgers</strong> <br><small>NL West</small><small>  (52-74)</small>
+          <br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+        </p>
+      </div>
+    </div>
+  </article>
+</div>
+  </div>
+</div>
+<div class="columns is-mobile">
+<div class="column">
+<div class="box">
+<article class="media">
+<div class="media-left">
+  <i class="bb-tor bb-5x"></i>
+</div>
+<div class="media-content">
+<div class="content">
+  <p>
+    <strong>Toronto Blue Jays</strong> <br><small>AL East</small><small>  (52-74)</small>
+    <br>
+    Lorem ipsum dolor sit amet, consectetur. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+  </p>
+</div>
+</div>
+</article>
+</div>
+</div>
+<div class="column">
+<div class="box">
+<article class="media">
+<div class="media-left">
+  <i class="bb-oak bb-5x"></i>
+</div>
+<div class="media-content">
+<div class="content">
+  <p>
+    <strong>Oakland Athletics</strong> <br><small>AL West</small><small>  (52-74)</small>
+    <br>
+    Lorem ipsum dolor sit amet, consectetur. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+  </p>
+</div>
+</div>
+</article>
+</div>
+</div>
+<div class="column">
+<div class="box">
+<article class="media">
+<div class="media-left">
+  <i class="bb-bos bb-5x"></i>
+</div>
+<div class="media-content">
+<div class="content">
+  <p>
+    <strong>Boston Red Sox</strong> <br><small>AL East</small><small>  (52-74)</small>
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+  </p>
+</div>
+</div>
+</article>
+</div>
+</div>
+</div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
+
+
+<style lang="sass">
+  @import '../node_modules/bulma/bulma.sass'
+  @import 'mq'
+
+
+</style>
